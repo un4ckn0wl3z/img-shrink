2412db71eeebed8c951fa0f5c90ae3f2f3d66722
un4ckn0wl3z <slickerth@gmail.com>
2020-08-23 14:26:34 +0700

    setup assets and env

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const { app, BrowserWindow } = require("electron");
 
+// set env
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isWin32 = process.platform === "win32" ? true : false;
+
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "ImageShrink",
     width: 500,
     height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev ? true : false,
   });
+
+  mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (isWin32) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
