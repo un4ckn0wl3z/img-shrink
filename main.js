68d917109dd88d1cd4ef11854af67b7779ee62fe
un4ckn0wl3z <slickerth@gmail.com>
2020-08-23 15:25:13 +0700

    struct the menu bar

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 // set env
 process.env.NODE_ENV = "development";
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isWin32 = process.platform === "win32" ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -21,19 +22,24 @@ function createMainWindow() {
   mainWindow.loadFile("./app/index.html");
 }
 
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: "About ImageShrink",
+    width: 300,
+    height: 300,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: false,
+    backgroundColor: "white",
+  });
+
+  aboutWindow.loadFile("./app/about.html");
+}
+
 app.on("ready", () => {
   createMainWindow();
 
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
-
-  globalShortcut.register("Ctrl+R", () => {
-    mainWindow.reload();
-  });
-
-  globalShortcut.register("Ctrl+Alt+I", () => {
-    mainWindow.toggleDevTools();
-  });
 
   mainWindow.on("ready", () => {
     mainWindow = null;
@@ -41,23 +47,35 @@ app.on("ready", () => {
 });
 
 const menu = [
-  ...(!isWin32
+  {
+    role: "fileMenu",
+  },
+  ...(isDev
     ? [
         {
-          role: "appMenu",
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
         },
       ]
     : []),
-  {
-    label: "File",
-    submenu: [
-      {
-        label: "Quit",
-        accelerator: "Ctrl+W",
-        click: () => app.quit(),
-      },
-    ],
-  },
+  ...(isWin32
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
 ];
 
 // if (isWin32) {
